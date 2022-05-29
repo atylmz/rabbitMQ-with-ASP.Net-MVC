31e3319719da1e895f7f46210b6b33aa52c34696
Ahmet Talha YILMAZ <ahmettalhayilmaz@outlook.com>
2022-05-30 01:23:40 +0300

    Initial

---
+++ RabbitMQ.MVC/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "http://127.17.0.2:15672/",
+                UserName = "guest",
+                Password = "guest"
+
+            };
+            using (var conecttion = factory.CreateConnection())
+            using (var channel = conecttion.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "hello",
+                    basicProperties: null,
+                    body: body);
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
